2f3c182269ad6528ba3aa349367d7359a5cd07c2
Aman Yadav <84045140+amanyadav91@users.noreply.github.com>
2024-03-04 23:58:46 +0530

    Adding how changes the data types from str to num etc

---
+++ Types Coercion.js
@@ -0,0 +1,32 @@
+// there are many data types in js that is:-
+
+// Number
+// String
+// Boolean
+// null
+// undefined
+// lets see how we can change from one form to other
+
+let a=1
+console.log(typeof a)  // Output: number
+console.log(a)
+console.log(typeof parseInt(a))
+
+// if we change into float then 
+b=1.8
+console.log(typeof b)
+console.log(typeof parseFloat(b))
+
+// now for the change the num to String
+console.log(typeof b.toString())
+
+// now adding the string & and number together
+let s=1;
+const y="Hello"
+console.log(s+y)
+
+let t=1
+const f="7"
+console.log(t+f)   // output: 17
+// to fixed it you can change it
+console.log(parseInt(f)+t)
